26b757da79655d4742bb0e96c52efba5677ba652
Waelpu <112546561+Waelpu@users.noreply.github.com>
2022-10-24 09:52:59 +0100

    Update insertionSort.js

--- insertionSort.js
+++ insertionSort.js
@@ -15,11 +15,3 @@ function insertionSort(array){
     return array;
 }
 
-console.log(insertionSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]));//Just For Test.
-
-
-
-/*******************************************************************************/
-/*******This is the implementation of the insertion sort with JavaScript********/
-/************You Have to Convert it into Algorithm it's easy :)*****************/
-/*******************************************************************************/
